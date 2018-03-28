42c68c1410e1cff5601c06cfddd35263056a7276
ezadrasyidi <33473592+ezadrasyidi@users.noreply.github.com>
2018-03-28 17:50:55 +0800

    + Model (studio 2015)

---
+++ GarageSaleMY/Models/Employerr.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GarageSaleMY.Models
+{
+    public class Employerr
+    {
+        public string name { get; set; }
+        public int staff_id { get; set; }
+        public string password { get; set; }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public int getStaffId()
+        {
+            return staff_id;
+        }
+
+        public string getPassword()
+        {
+            return password;
+        }
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+
+        public void setStaffId(int staff_id)
+        {
+            this.staff_id = staff_id;
+        }
+
+        public void setPassword(string password)
+        {
+            this.password = password;
+        }
+
+
+    }
+}
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GarageSaleMY.Models
+{
+    public class Employerr
+    {
+    }
+}
